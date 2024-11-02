f42448935c66405b4368ef4b26cc2bfe9794555e
Scott <scottevans85@hotmail.com>
2024-11-02 01:31:15 +0000

    # Please include a useful commit message! with python! ['.git/hooks/prepare-commit-msg', '.git/COMMIT_EDITMSG', 'message']

--- final_room.py
+++ final_room.py
@@ -1,5 +1,6 @@
 # NOT IN USE
 
+
 def final_room():
     print("Welcome to the Final Boss!")
     questions = {
@@ -7,36 +8,25 @@ def final_room():
             {
                 "easy_q": "Declare a variable 'x' with the string 'Hello, World!'",
                 "easy_ans": "x = 'Hello, World!'",
-                "easy_output": "I hope you got this correct, the answer is 'x = \'Hello, World!\'"
+                "easy_output": "I hope you got this correct, the answer is 'x = 'Hello, World!'",
             },
             {
                 "med_q": "True or False: You can handle exceptions in Python using try and catch blocks.",
                 "med_ans": "False",
-                "med_output": "The answer is False, In Python we use try and except blocks"
+                "med_output": "The answer is False, In Python we use try and except blocks",
             },
-            {
-                "hard"
-            }
+            {"hard"},
         ],
         "DevOps": [
-            {
-                "easy_q": ""
-
-            },
-            {
-                "medium_q": ""
-            },
-            {
-                "hard_q": "",
-                "hard_ans": "",
-                "hard_output": ""
-            }
-        ]
+            {"easy_q": ""},
+            {"medium_q": ""},
+            {"hard_q": "", "hard_ans": "", "hard_output": ""},
+        ],
         "Final": [
             {
                 "hard_q": "What is DevOps?",
                 "hard_ans": "42",
-                "hard_output": "The Answer to the Ultimate Question of Life, The Universe, and Everything.. is 42"
+                "hard_output": "The Answer to the Ultimate Question of Life, The Universe, and Everything.. is 42",
             }
-        ]
+        ],
     }
